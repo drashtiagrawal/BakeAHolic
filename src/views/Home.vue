6ff50b1cc9cc8c001148781d8343d14991c40423
Shubhang <shubhang@officebrain.com>
2019-11-14 19:45:06 +0530

    Items-layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,29 +5,61 @@
             <Header>
                 <h2 class="header"> Bake-A-Holic</h2>
             </Header>
-            <Layout :style="{padding: '0 50px'}">
+            <Layout>
+              <Sider hide-trigger :style="{background: '#fff'}">
+                    <Menu active-name="1-2" theme="light" width="" :open-names="['1']">
+                        <Submenu name="1">
+                            <template slot="title">
+                                <Icon type="ios-navigate"></Icon>
+                                Item 1
+                            </template>
+                            <!-- <MenuItem name="1-1">Option 1</MenuItem>
+                            <MenuItem name="1-2">Option 2</MenuItem>
+                            <MenuItem name="1-3">Option 3</MenuItem> -->
+                        </Submenu>
+                        <Submenu name="2">
+                            <template slot="title">
+                                <Icon type="ios-keypad"></Icon>
+                                Item 2
+                            </template>
+                            <!-- <MenuItem name="2-1">Option 1</MenuItem>
+                            <MenuItem name="2-2">Option 2</MenuItem> -->
+                        </Submenu>
+                        <Submenu name="3">
+                            <template slot="title">
+                                <Icon type="ios-analytics"></Icon>
+                                Item 3
+                            </template>
+                            <!-- <MenuItem name="3-1">Option 1</MenuItem>
+                            <MenuItem name="3-2">Option 2</MenuItem> -->
+                        </Submenu>
+                    </Menu>
+                </Sider>
+            <Layout :style="{padding: '0 50px'}">  
                 <Breadcrumb :style="{margin: '16px 0'}">
                     <BreadcrumbItem>Home</BreadcrumbItem>
                     <BreadcrumbItem>Items</BreadcrumbItem>
+                    <Icon type="md-cart" size=24 style="float: right; margin-right:10%;" @click="showCart" />
                 </Breadcrumb>
-                <Content :style="{padding: '24px 0', minHeight: '280px', background: '#fff'}">
+                
+                
+                <Content :style="{padding: '24px 0', minHeight: '500px', background: '#fff'}">
                     <Layout>
-                        <Sider hide-trigger :style="{background: '#fff'}">
-                        </Sider>
-                        <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
+                        <Content :style="{padding: '24px', minHeight: '500px', background: '#fff'}">
                           <Row style="background:#eee;padding:20px">
                             <Col v-for="key in items" :key="key.itemName" span="6" offset="2">
                                 <Card :bordered="false">
                                     <p slot="title">{{ key.itemName }}</p>
                                     <img src="../assets/bakeaholic.jpeg">
                                     <p>Rs: {{ key.itemPrice }}</p>
-                                    <Button type="primary" style="margin-top: 10px;">Add to cart</Button>
+                                    <Button  type="primary" style="margin-top: 10px;" @click="addToCart(key)">Add to cart</Button>
                                 </Card>
                             </Col>
                         </Row>
                         </Content>
                     </Layout>
                 </Content>
+            </Layout>
             </Layout>
             <Footer class="layout-footer-center">2011-2016 &copy; TalkingData</Footer>
         </Layout>
@@ -44,6 +76,28 @@ export default {
   data(){
     return{
       items: [],
+      selectedItem: ''
+    }
+  },
+  methods: {
+    showCart(){
+      console.log("Called")
+    },
+    addToCart(key){
+      console.log(key.itemName)
+      let cartData = {
+        "itemName": key.itemName,
+        "number": "1"
+      }
+      axios({
+        method: 'post',
+        data: cartData,
+        url: 'http://3.85.235.191/v1/addItem'
+      }).then((response) => {
+        console.log("Response:", response)
+      }).catch((err) => {
+        console.log("Error Calling Service", err)
+      })
     }
   },
   mounted(){
@@ -78,6 +132,28 @@ export default {
 <style scoped>
 .header{
   color: aliceblue
+}
+.layout{
+    border: 1px solid #d7dde4;
+    background: #f5f7f9;
+    position: relative;
+    border-radius: 4px;
+    overflow: hidden;
+}
+.layout-logo{
+    width: 100px;
+    height: 30px;
+    background: #5b6270;
+    border-radius: 3px;
+    float: left;
+    position: relative;
+    top: 15px;
+    left: 20px;
+}
+.layout-nav{
+    width: 420px;
+    margin: 0 auto;
+    margin-right: 20px;
 }
 </style>
 
